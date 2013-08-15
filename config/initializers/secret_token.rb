2b001692f66e2b9abda5a49fab4687a8aaba729e
Beth Tenorio <betsky98@gmail.com>
2013-08-15 16:02:27 -0400

    Add navigation and filter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shmoogle::Application.config.secret_key_base = '68e05ddeb8530f2ccbc3f297941322b99cc7592f420f55b2362a3b6e028fa5e4c63244d32a0f7f0a44d5b2c67b71c411826144a7db13d21cf85c118f4d9675f6'
